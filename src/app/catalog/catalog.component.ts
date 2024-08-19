2781e1290b8e902ecbcaa6e6ef9bce262fbaf2fc
Natheer <Natheer.Shade@csgi.com>
2024-08-19 16:29:26 +0200

    Catalog Update

--- src/app/catalog/catalog.component.ts
+++ src/app/catalog/catalog.component.ts
@@ -4,6 +4,8 @@ import { CartService } from '../cart/cart.service';
 import { WishlistService } from '../wishlist/wishlist.service';
 import { HeaderComponent } from '../header/header.component';
 import { CommonModule, NgFor } from '@angular/common';
+import { Product } from './product.model';
+import { ProductService } from './product.service';
 
 @Component({
   selector: 'app-catalog',
@@ -14,18 +16,16 @@ import { CommonModule, NgFor } from '@angular/common';
 })
 export class CatalogComponent {
 
-  products = [
-    { productId: 1, name: 'Plant 1', description: 'Beautiful indoor plant', price: 20, image: '../assets/plant1.jpg', stockQuantity: 1, category: '' },
-    { productId: 2, name: 'Plant 2', description: 'Indoor Cheese', price: 500, image: '../assets/plant2.jpg', stockQuantity: 1, category: '' },
-    { productId: 3, name: 'Plant 3', description: 'Succulent plant', price: 15, image: '../assets/plant3.jpg', stockQuantity: 1, category: '' },
-    { productId: 4, name: 'Plant 4', description: 'Bonzai', price: 50, image: '../assets/plant4.jpg', stockQuantity: 1, category: '' },
-    { productId: 5, name: 'Plant 5', description: 'Succulent unique', price: 40, image: '../assets/plant6.jpg', stockQuantity: 1, category: '' },
-    // Add more products as needed
-  ];
+  products: Product[] = [];
 
-  constructor(private router: Router, private cartService: CartService, private wishlistService: WishlistService) { }
+  constructor(private router: Router, private cartService: CartService, private wishlistService: WishlistService, 
+    private productService: ProductService
+  ) { }
 
   ngOnInit(): void {
+    this.productService.getAllProducts().subscribe(products => {
+      this.products = products;
+    });
   }
 
   addToCart(product: any) {
--- src/app/catalog/product.model.ts
+++ src/app/catalog/product.model.ts
@@ -3,7 +3,7 @@ export interface Product {
     name: string;
     description: string;
     price: number;
-    image: string;
+    imageUrl: string;
     stockQuantity: number;
     category: string
 }
