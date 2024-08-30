497df1b05a5b09af59dbb40bc57fd5dfd9f33559
Natheer <Natheer.Shade@csgi.com>
2024-08-30 22:07:34 +0200

    Update Profile validation.

--- src/app/profile/update-profile/update-profile.component..ts
+++ src/app/profile/update-profile/update-profile.component..ts
@@ -2,6 +2,7 @@ import { Component, DestroyRef, inject } from "@angular/core";
 import { CustomerService } from "../../customer/customer.service";
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from "@angular/forms";
 import { Customer } from "../../customer/customer.model";
+import { HttpErrorResponse } from "@angular/common/http";
 
 @Component({
   selector: 'app-update-profile',
@@ -17,6 +18,9 @@ export class UpdateProfileComponent {
 
   private destroyRef = inject(DestroyRef);
 
+  customerEmail: string | undefined | null;
+  customerNumber: string | undefined | null;
+
   // customer: Customer = this.customerservice.customer;
   // customer = this.customerService.getCustomerLocal();
 
@@ -26,7 +30,7 @@ export class UpdateProfileComponent {
       // Parse the JSON string to a Customer object
       const customer: Customer = JSON.parse(storedCustomer);
       //console.log(customer);
-      return(customer)
+      return (customer)
     }
     return null;
   }
@@ -67,16 +71,45 @@ export class UpdateProfileComponent {
       dateOfBirth: this.form.value.dateOfBirth
     }
 
-    const subscription = this.customerService.updateCustomer(customer).subscribe({
-      next: (customer: Customer) => {
-        //this.customerservice.customer = customer;
-        localStorage.setItem('customer', JSON.stringify(customer));
-        alert('Successful Update');
-      },
-      error: () => {
-        alert('Error');
+
+    const subscription = this.customerService.getCustomer(this.form.value.email).subscribe({
+      next: (customerExist: Customer) => {
+        if (customerExist) {
+          //alert('Email already taken');
+          if (customerExist.email === this.getCustomer()?.email) {
+            this.customerEmail = null;
+          } else {
+            this.customerEmail = customerExist.email;
+            return;
+          }
+        }
+        // if (this.form.value.mobileNumber !== null) {
+        //   this.customerService.getCustomerByMobileNumber(this.form.value.mobileNumber).subscribe({
+        //     next: (customerNew: Customer) => {
+        //       if (customerNew) {
+        //         //alert('Email already taken');
+        //         if (customerNew.mobileNumber === this.getCustomer()?.mobileNumber) {
+        //           this.customerNumber = null;
+        //         } else {
+        //           this.customerNumber = customerNew.mobileNumber;
+        //           return;
+        //         }
+        //       }
+        //     }
+        //   });
+        // }
+        this.customerService.updateCustomer(customer).subscribe({
+          next: (customer: Customer) => {
+            //this.customerservice.customer = customer;
+            localStorage.setItem('customer', JSON.stringify(customer));
+            alert('Successful Update');
+          },
+          error: () => {
+            alert('Error');
+          }
+        })
       }
-    })
+    });
 
     this.destroyRef.onDestroy(() => {
       subscription.unsubscribe();
